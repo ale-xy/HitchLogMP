5a4c41f103d6542092efafc24c3486eb00fe1f32
alexeykrichun <a.krichun@lipt-soft.ru>
2026-05-06 01:50:58 +0300

    Add web target

--- composeApp/src/jsMain/kotlin/org/gmautostop/hitchlogmp/FirebaseInit.kt
+++ composeApp/src/jsMain/kotlin/org/gmautostop/hitchlogmp/FirebaseInit.kt
@@ -5,15 +5,6 @@ import dev.gitlive.firebase.FirebaseOptions
 import dev.gitlive.firebase.initialize
 
 fun initializeFirebaseForWeb() {
-    console.log("Firebase config:", js("""({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        gcmSenderId: process.env.FIREBASE_GCM_SENDER_ID,
-        applicationId: process.env.FIREBASE_APPLICATION_ID
-    })"""))
-    
     Firebase.initialize(
         options = FirebaseOptions(
             apiKey = FirebaseConfig.apiKey,
--- composeApp/webpack.config.d/firebase-env.js
+++ composeApp/webpack.config.d/firebase-env.js
@@ -7,14 +7,8 @@ const path = require('path');
 const localPropertiesPath = path.resolve(__dirname, '../../../../local.properties');
 const firebaseConfig = {};
 
-console.log('=== Firebase Webpack Config ===');
-console.log('__dirname:', __dirname);
-console.log('localPropertiesPath:', localPropertiesPath);
-console.log('File exists:', fs.existsSync(localPropertiesPath));
-
 if (fs.existsSync(localPropertiesPath)) {
     const properties = fs.readFileSync(localPropertiesPath, 'utf8');
-    console.log('Properties file length:', properties.length);
     properties.split('\n').forEach(line => {
         const trimmed = line.trim();
         if (trimmed && !trimmed.startsWith('#')) {
@@ -26,26 +20,18 @@ if (fs.existsSync(localPropertiesPath)) {
                 // Convert camelCase to SNAKE_CASE
                 const envKey = 'FIREBASE_' + propName.replace(/([A-Z])/g, '_$1').toUpperCase().replace(/^_/, '');
                 firebaseConfig[envKey] = value;
-                console.log(`Loaded ${key} -> ${envKey} = ${value.substring(0, 10)}...`);
             }
         }
     });
-    console.log('Final firebaseConfig:', firebaseConfig);
-} else {
-    console.error('local.properties not found at:', localPropertiesPath);
 }
 
-const definePluginConfig = {
-    'process.env.FIREBASE_API_KEY': JSON.stringify(firebaseConfig.FIREBASE_API_KEY || ''),
-    'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(firebaseConfig.FIREBASE_AUTH_DOMAIN || ''),
-    'process.env.FIREBASE_PROJECT_ID': JSON.stringify(firebaseConfig.FIREBASE_PROJECT_ID || ''),
-    'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(firebaseConfig.FIREBASE_STORAGE_BUCKET || ''),
-    'process.env.FIREBASE_GCM_SENDER_ID': JSON.stringify(firebaseConfig.FIREBASE_GCM_SENDER_ID || ''),
-    'process.env.FIREBASE_APPLICATION_ID': JSON.stringify(firebaseConfig.FIREBASE_APPLICATION_ID || '')
-};
-
-console.log('DefinePlugin config:', definePluginConfig);
-
 config.plugins.push(
-    new webpack.DefinePlugin(definePluginConfig)
+    new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(firebaseConfig.FIREBASE_API_KEY || ''),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(firebaseConfig.FIREBASE_AUTH_DOMAIN || ''),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(firebaseConfig.FIREBASE_PROJECT_ID || ''),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(firebaseConfig.FIREBASE_STORAGE_BUCKET || ''),
+        'process.env.FIREBASE_GCM_SENDER_ID': JSON.stringify(firebaseConfig.FIREBASE_GCM_SENDER_ID || ''),
+        'process.env.FIREBASE_APPLICATION_ID': JSON.stringify(firebaseConfig.FIREBASE_APPLICATION_ID || '')
+    })
 );
